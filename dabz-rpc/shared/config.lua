968364ee9c72b6f126ca52618924925eb4e59918
Dabz <144912368+DabzDev@users.noreply.github.com>
2024-05-26 06:33:23 -0500

    Add files via upload

---
+++ dabz-rpc/shared/config.lua
@@ -0,0 +1,14 @@
+Config = {
+    DiscordAppID = 11111111111111, -- discord bot app id https://discord.com/developers/docs/intro
+    AppBigAssetID = 'dabz_development',
+    AppBigAssetText = 'dabz_development',
+    AppSmallAssetID = 'dabz_development',
+    AppSmallAssetText = 'HELLO GUYS',
+    FirstButtonPlaceholder = 'DABZ SCRIPTS',
+    FirstButtonLink = 'https://discord.gg/cfxdev',
+    SecondButtonPlaceholder = 'Discord',
+    SecondButtonLink = 'https://discord.gg/cfxdev',
+
+    UpdateEvery = 60000, -- In ms. (60000ms = 1 minute)
+    Template = '{Citizens} | ID: {ID} | NAME: {NAME}' -- {NAME}, {ID}, {Citizens}
+}
